4d84664b6d8d4958ab7b7912f077e963f4f0dff5
goaldae <goaldae@naver.com>
2019-01-30 20:17:55 +0900

    2 test commit

---
+++ src/p0131/d01.java
@@ -0,0 +1,10 @@
+package p0131;
+
+public class d01 {
+
+	public static void main(String[] args) {
+		System.out.println("good stuff");
+
+	}
+
+}
--- src/practice_java/p0130.java
+++ src/practice_java/p0130.java
@@ -3,8 +3,8 @@ package practice_java;
 public class p0130 {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO");
-
+		int[] arr1 = new int[3];
+		System.out.println(arr1);
 	}
 
 }
