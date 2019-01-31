3a7a105c136086129c964bd78554c866ebb58ef3
goaldae <goaldae@naver.com>
2019-01-31 14:11:48 +0900

    gcd function

--- src/practice_java/p0130.java
+++ src/practice_java/p0130.java
@@ -1,10 +1,24 @@
 package practice_java;
-
+import java.util.Scanner;
+//최대공약수 구하기 함수
 public class p0130 {
 
 	public static void main(String[] args) {
-		int[] arr1 = new int[3];
-		System.out.println(arr1);
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		int res = gcd(x, y);
+		System.out.println(res);
+	}
+	
+	public static int gcd(int p, int q){
+		if(q==0){
+			return p;
+		}else{
+			return gcd(q, p%q);
+		}
 	}
 
 }
