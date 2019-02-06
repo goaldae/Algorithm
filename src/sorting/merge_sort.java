5a4151d49ddbe33d895b49e966e6046a1e1304fb
goaldae <goaldae@naver.com>
2019-02-06 15:12:04 +0900

    test commit

---
+++ src/sorting/merge_sort.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public class merge_sort {
+	
+	public static int i = 1;
+	public static void main(String[] args) {
+		int i=0;
+		int[]arr = {1,2};
+		System.out.println(arr[i++]);
+		System.out.println(arr[i]);
+	}
+
+}
