5d128be69a5f21d2436d31a7d02e2f21120cf756
goaldae <goaldae@naver.com>
2019-02-10 23:30:37 +0900

    Complete Heap Sort

--- src/sorting/HeapSort.java
+++ src/sorting/HeapSort.java
@@ -7,25 +7,43 @@ public class HeapSort {
         
         HeapSort ob = new HeapSort();
         
-        for(int i = (n/2)-1; i>=0; i--){
-        	ob.heapify(data, i);
+        for(int i = (n-1)/2; i>=0; i--){
+        	ob.heapify(data, i, n);
         }
+        
         for(int i = 0; i<n; i++){
-        	System.out.println(data[i]);
+        	System.out.printf("%d ", data[i]);
+        }
+        System.out.println();
+        
+        for(int i = n-1; i>=0; i--){
+        	ob.sort(data, i);
+        }
+        
+        for(int i = 0; i<n; i++){
+        	System.out.printf("%d ", data[i]);
         }
         
     } 
     
-    void heapify(int[] data, int i){
-    	System.out.println(data[i]);	
+    void sort(int[] data, int i){
+    	int temp;
+    	temp = data[0];
+    	data[0] = data[i];
+    	data[i] = temp;
+    	
+    	heapify(data, 0, i);
+    }
+    
+    void heapify(int[] data, int i, int n){
     	int l = i * 2 + 1; //왼쪽 자식 노드
     	int r = i * 2 + 2; //오른쪽 자식 노드
     	int largest = i;
     	int temp;
     	
-    	if(data[l] > data[largest] && data[l] >= data[r]){ //부모보다 왼쪽 자식이 큰 경우
+    	if(r < n && data[l] > data[largest] && data[l] >= data[r]){ //부모보다 왼쪽 자식이 큰 경우
     		largest = l;
-    	//}else if(data[r] > data[largest] && data[r] >= data[l]){
+    	}else if(r < n && data[r] > data[largest] && data[r] >= data[l] ){
     		largest = r;
     	}
     	
@@ -34,7 +52,7 @@ public class HeapSort {
     		data[i] = data[largest];
     		data[largest] = temp;
     		
-    		heapify(data, largest);
+    		heapify(data, largest, n);
     	}
     }
 }
