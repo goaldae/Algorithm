5a0e0ab4c08df744a3e5c5224bb753637f0811e0
goaldae <goaldae@naver.com>
2019-02-11 15:16:06 +0900

    Modify Heap Sort(Complete)

---
+++ .bash_history
@@ -0,0 +1,46 @@
+echo $HOME
+ls
+cd ~
+ls
+cd ~
+ls
+git log
+git status
+git commit -m "Complete Quick Sort"
+git config --global user.name goaldae
+git config --global user.email goaldae@naver.com
+git config
+git config --get
+git config -get
+git commit -m "Complete Quick Sort"
+git push
+cd ~
+ls
+cd ~
+ls
+git status
+git add src/sorting/quick_sort.java
+git status
+git commit -m "Modify Quick Sort"
+git status
+git add src/sorting/HeapSort.java
+git status
+git commit -m "Make HeapSort"
+cd ~
+ls
+git statsu
+git status
+git push
+git status
+git add src/sorting/quick_sort.java
+git commit -m "Modify Quick Sort"
+git add src/sorting/HeapSort.java
+git commit -m "Complete Heap Sort"
+git push
+git status
+git add .bash_history
+git status
+git add .gitconfig
+git status
+git commit
+git log
--- src/sorting/HeapSort.java
+++ src/sorting/HeapSort.java
@@ -2,37 +2,45 @@ package sorting;
 
 public class HeapSort {
     public static void main(String args[]) { 
-        int data[] = {12, 11, 13, 5, 6, 7}; 
+        int data[] = {7, 13, 6, 5, 11, 12}; 
         int n = data.length; 
         
         HeapSort ob = new HeapSort();
         
-        for(int i = (n-1)/2; i>=0; i--){
-        	ob.heapify(data, i, n);
-        }
+        ob.buildHeap(data, n); //heap tree 만들기
         
-        for(int i = 0; i<n; i++){
-        	System.out.printf("%d ", data[i]);
-        }
-        System.out.println();
-        
-        for(int i = n-1; i>=0; i--){
-        	ob.sort(data, i);
-        }
-        
-        for(int i = 0; i<n; i++){
-        	System.out.printf("%d ", data[i]);
-        }
-        
+        printData(data, n);
+       
+        ob.heapSort(data, n-1); //완성된 heap tree를 heap sort하기
+      
+        printData(data, n);        
     } 
     
-    void sort(int[] data, int i){
+    public static void printData(int[] data, int n){
+    	 for(int i = 0; i<n; i++){
+         	System.out.printf("%d ", data[i]);
+         }
+         System.out.println();
+    }
+    
+    void buildHeap(int[] data, int n){
+    	for(int i = (n-1)/2; i>=0; i--){
+        	heapify(data, i, n);
+        }
+    }
+    
+    void heapSort(int[] data, int i){
+    	if(i<=0){
+    		return;
+    	}
+    	
     	int temp;
     	temp = data[0];
     	data[0] = data[i];
     	data[i] = temp;
     	
     	heapify(data, 0, i);
+    	heapSort(data, i-1);
     }
     
     void heapify(int[] data, int i, int n){
@@ -41,9 +49,10 @@ public class HeapSort {
     	int largest = i;
     	int temp;
     	
-    	if(r < n && data[l] > data[largest] && data[l] >= data[r]){ //부모보다 왼쪽 자식이 큰 경우
+    	if(l < n && data[l] > data[largest]){ //부모보다 왼쪽 자식이 큰 경우
     		largest = l;
-    	}else if(r < n && data[r] > data[largest] && data[r] >= data[l] ){
+    	}
+    	if(r < n && data[r] > data[largest]){
     		largest = r;
     	}
     	
