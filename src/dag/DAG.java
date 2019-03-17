be7a6e1a56a3fb3e54ffaba63714eb2b6add8ab9
goaldae <goaldae@naver.com>
2019-03-17 11:41:21 +0900

    Complete DAG

--- src/dag/DAG.java
+++ src/dag/DAG.java
@@ -1,3 +1,15 @@
+		/* 입력 예시
+8 9
+0 1
+0 2
+0 3
+3 4
+4 5
+2 5
+2 6
+1 6
+6 7
+		*/
 package dag;
 
 import java.util.*;
@@ -7,32 +19,41 @@ public class DAG {
 	static int E;
 	static int N;
 	static LinkedList<Integer>[] a;
-	static boolean[] visited;
-	static Queue<Integer> queue;
+	
+	static Queue<Integer> queue = new LinkedList<>();
 	
 	static int[] nodeCount; //자신에게 들어온 화살표 세는 변수
-	static int[] result; //위상정렬된 배열
+	static ArrayList<Integer> result = new ArrayList<>(); //위상정렬된 배열
 	
 	public static void addEdge(int n1, int n2){
 		a[n1].add(n2);
-		nodeCount[n2]++;
+		nodeCount[n2] += 1;
 	}
 	
-	public static void topologicalSort(int s){
-		visited[s] = true;
-		queue.add(s);
-		
-		nodeCount[s]--;
+	public static void topologicalSort(){
+		for(int i = 0; i < N; i++){
+			if(nodeCount[i]==0) queue.add(i);
+		}
 		
 		while(!queue.isEmpty()){
 			int temp = queue.poll();
-			System.out.println(temp+" ");
+			result.add(temp);
 			
-			
+			for(int i =0; i < a[temp].size(); i++){
+				nodeCount[a[temp].get(i)]--;
+				if(nodeCount[a[temp].get(i)]==0){
+					queue.add(a[temp].get(i));
+				}
+			}			
+		}	
+		
+		for(int i = 0; i < result.size(); i++){
+			System.out.print(result.get(i)+" ");
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
+		//입력및 선언부분//
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
@@ -42,6 +63,9 @@ public class DAG {
 		
 		N = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
+		a = new LinkedList[N];
+		result = new ArrayList<>();
+		nodeCount = new int[N];
 		
 		for(int i = 0; i < N; i++){
 			a[i] = new LinkedList<>();
@@ -49,8 +73,11 @@ public class DAG {
 		
 		for(int i = 0; i < E; i++){
 			String[] tempArr = br.readLine().split(" ");
-			addEdge(Integer.parseInt(tempArr[0]), Integer.parseInt(tempArr[0]));
+			addEdge(Integer.parseInt(tempArr[0]), Integer.parseInt(tempArr[1]));
 		}
+		/////////////////
+
+		topologicalSort();
 				
 	}
 
