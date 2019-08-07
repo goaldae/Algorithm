8ce174978cdd70eb5e230d868ebe741432167f87
goaldae <goaldae@naver.com>
2019-08-07 17:05:16 +0900

    Add LIS11055, LIS11053

---
+++ src/baekjoon/LIS11055.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class LIS11055 {
+    static int N;
+    static int[] arr = new int[1001];
+    static int[] dp = new int[1001];
+
+    static int maxValue(int k){
+        int max;
+        int res = 0;
+        for(int i = 0; i <= k; i++){
+            max = 0;
+            for(int j = 0; j<i; j++){
+                if(arr[i]>arr[j] && max<dp[j]){
+                    max = dp[j];
+                }
+            }
+            dp[i] = max + dp[i];
+            if(dp[i]>res) res = dp[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        String input = br.readLine();
+        st = new StringTokenizer(input, " ");
+        for(int i = 0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i<N; i++){ //dp ÃÊ±âÈ­
+            dp[i] = arr[i];
+        }
+
+        System.out.println(maxValue(N-1));
+    }
+}
