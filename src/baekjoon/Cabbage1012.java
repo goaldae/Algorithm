9e98ffc49de86f28d83fb34caeec5e5ea36759ac
goaldae <goaldae@naver.com>
2019-08-21 11:41:28 +0900

    Add Cabbage

--- src/baekjoon/Cabbage1012.java
+++ src/baekjoon/Cabbage1012.java
@@ -20,28 +20,28 @@ public class Cabbage1012 {
     static LinkedList<Pos3> queue = new LinkedList<>();
     static boolean[][] visited = new boolean[51][51];
     static int[][] direction = {
-            {-1, 0, -1, 0},
-            {0, 1, 0, -1}};
+            {-1, 0, 1, 0}, //x
+            {0, 1, 0, -1}};//y
     static int res = 0;
 
     static void bfs(int x, int y){
-        visited[x][y] = true;
+        visited[y][x] = true;
         Pos3 p = new Pos3(x, y);
         queue.add(p);
 
         while(!queue.isEmpty()){
             p = queue.poll();
-            for(int i = 0; i<2; i++){
-                for(int j = 0; j<4; j++){
-                    int tempM = p.x+direction[i][j];
-                    int tempN = p.y+direction[i][j];
-                    if(tempN>-1&&tempM>-1&&visited[tempM][tempN]==false &&cabbage[tempM][tempN]==1){
-                        queue.add(new Pos3(tempM, tempN));
-                        visited[tempM][tempN]= true;
-                    }
+            for(int j = 0; j<4; j++){
+                int tempX = p.x+direction[0][j];
+                int tempY = p.y+direction[1][j];
+                if(tempX<N&& tempY<M && tempY>-1&&tempX>-1&& visited[tempY][tempX]==false && cabbage[tempY][tempX]==1){
+                    queue.add(new Pos3(tempX, tempY));
+                    visited[tempY][tempX]= true;
+                    cabbage[tempY][tempX] = 9;
                 }
             }
         }
+
         res++;
     }
     public static void main(String[] args){
@@ -50,23 +50,30 @@ public class Cabbage1012 {
         T = sc.nextInt();
 
         for(int i = 0; i<T; i++){
-            M = sc.nextInt();
             N = sc.nextInt();
+            M = sc.nextInt();
             P = sc.nextInt();
             for(int j = 0; j<P; j++){
                 x = sc.nextInt();
                 y = sc.nextInt();
                 cabbage[y][x] = 1;
             }
-            for(int k = 0; k<N; k++){
-                for(int l = 0; l<M; l++){
-                    if(visited[k][l] == false&&cabbage[k][l]==1){
-                        bfs(k, l);
+
+
+            for(int k = 0; k<M; k++){
+                for(int l = 0; l<N; l++){
+                    if(cabbage[k][l]==1 && visited[k][l] == false){
+                        bfs(l, k);
                     }
                 }
 
             }
-        System.out.println(res);
+            System.out.println(res);
+
+            cabbage = new int[51][51]; //변수 초기화
+            queue = new LinkedList<>();
+            visited = new boolean[51][51];
+            res = 0;
         }
     }
 }
